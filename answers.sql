e0308381ea0af1d3b48c9cc0b8835c9d4642b8ad
mwarabu200 <susanmwarabu@gmail.com>
2025-05-11 17:17:51 +0300

    Update answers.sql
    WEEK7 assignment

--- answers.sql
+++ answers.sql
@@ -1,3 +1,4 @@
+
 USE salesdb;
 
 
@@ -19,3 +20,34 @@ VALUES
 (103, "Emily Clark","Phone");
 
 SELECT* FROM normalisedproduct;
+
+
+
+2ND NF
+
+CREATE TABLE normalisedorder(
+ ORDERID INT PRIMARY KEY AUTO_INCREMENT,
+CustomerName VARCHAR(100)
+);
+
+INSERT INTO normalisedorder(ORDERID,CustomerName)
+VALUES(101, "John Doe"),
+(102,"Jane Smith"),
+(103, "Emily Clark"); 
+
+CREATE TABLE normalisedOrderDetails(
+ORDERID INT,
+product VARCHAR(100),
+Quantity INT,
+PRIMARY KEY (orderID, product),
+FOREIGN KEY (OrderID) REFERENCES normalisedOrders(OrdersID)
+);
+
+INSERT INTO normalisedOrderDetails (OrderID, product, Quantity)
+VALUES(101, "Laptop", 2),
+(101, "Mouse", 1),
+(102, "Tablet", 3),
+(102, "Keyboard", 1),
+(102, "Mouse", 2),
+(103, "Phone", 1);
+
